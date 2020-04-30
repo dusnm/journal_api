b90c82246ea4c66774aaa6749d618537ce12b6a5
Dusan Mitrovic <dusan1134@gmail.com>
2020-05-01 01:27:28 +0200

    Improve image upload

--- src/Services/DiskImageUploadService.php
+++ src/Services/DiskImageUploadService.php
@@ -8,17 +8,8 @@ use Psr\Http\Message\UploadedFileInterface;
 use function App\Helpers\randomString;
 use function App\Helpers\url;
 
-define('UPLOAD_DIRECTORY', '/public/images/'.date('d-m-Y'));
-
 class DiskImageUploadService implements ImageUploadInterface
 {
-    private string $uploadPath;
-
-    public function __construct()
-    {
-        $this->uploadPath = __DIR__.'/../../'.UPLOAD_DIRECTORY;
-    }
-
     /**
      * Generates a random file name for the uploaded file and moves the file to the upload directory
      *
@@ -28,18 +19,20 @@ class DiskImageUploadService implements ImageUploadInterface
     */
     public function upload(UploadedFileInterface $uploadedFile): string
     {
+        $uploadPath =  __DIR__.'/../../public/images/'.date('d-m-Y');
+
         // Create a directory to store the uploaded file based on the current date
-        if (!file_exists($this->uploadPath)) {
-            mkdir($this->uploadPath, 0755, true);
+        if (!file_exists($uploadPath)) {
+            mkdir($uploadPath, 0755, true);
         }
 
         $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
         $basename = randomString(15);
         $filename = sprintf('%s.%0.8s', $basename, $extension);
-        $filePath = $this->uploadPath.DIRECTORY_SEPARATOR.$filename;
+        $filePath = $uploadPath.DIRECTORY_SEPARATOR.$filename;
 
         $uploadedFile->moveTo($filePath);
-
-        return url(str_replace('/public', '', UPLOAD_DIRECTORY).DIRECTORY_SEPARATOR.$filename);
+    
+        return url(strstr($filePath, '/images'));
     }
 }
