430f530a1007680567944d1c5b9d1a462a86f883
dusnm <dusan1134@gmail.com>
2020-05-20 23:19:28 +0200

    Add doc comments to JournalService

--- src/Services/JournalService.php
+++ src/Services/JournalService.php
@@ -11,25 +11,42 @@ use App\Models\Journal;
 
 class JournalService
 {
-    private array $eagerLoadedRelationships = [
+    /* A list of models to eager load */
+    private array $eagerLoadedModels = [
         'images',
     ];
 
+    /**
+     * Get a single Journal record from the database identified by id and belonging to the user making a request
+     *
+     * @param \App\DTO\Journal\ReadByIdJournalDTO $readByIdJournalDTO
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     *
+     * @return \App\Models\Journal The retrieved Journal record
+     */
     public function readById(ReadByIdJournalDTO $readByIdJournalDTO)
     {
         return Journal::query()
-            ->with($this->eagerLoadedRelationships)
+            ->with($this->eagerLoadedModels)
             ->where([
                 ['id', '=', $readByIdJournalDTO->id],
                 ['user_id', '=', $readByIdJournalDTO->userId]
             ])->firstOrFail();
     }
 
+    /**
+     * Get a paginated list of Journal records from the database belonging to the user making a request
+     *
+     * @param \App\DTO\Journal\ReadJournalDTO $readJournalDTO
+     *
+     * @return array paginated list of retrieved Journal records
+     */
     public function read(ReadJournalDTO $readJournalDTO): array
     {
         return [
             'journals' => Journal::query()
-                ->with($this->eagerLoadedRelationships)
+                ->with($this->eagerLoadedModels)
                 ->where('user_id', '=', $readJournalDTO->userId)
                 ->orderBy('created_at', 'DESC')
                 ->skip(($readJournalDTO->page - 1) * $readJournalDTO->rowsPerPage)
@@ -42,7 +59,14 @@ class JournalService
             ),
        ];
     }
-
+    
+    /**
+     * Create a new Journal record in the database belonging to the user requesting creation
+     *
+     * @param \App\DTO\JournalCreateJournalDTO $createJournalDTO
+     *
+     * @return \App\Models\Journal an instance of the newly created Journal record
+     */
     public function create(CreateJournalDTO $createJournalDTO): Journal
     {
         return Journal::query()->create([
@@ -51,7 +75,14 @@ class JournalService
             'user_id' => $createJournalDTO->userId,
         ]);
     }
-
+    
+    /**
+     * Update the Journal record in the database belonging to the user requesting an update
+     *
+     * @param \App\DTO\Journal\UpdateJournalDTO $updateJournalDTO
+     *
+     * @return int 1 on success 0 on failure
+     */
     public function update(UpdateJournalDTO $updateJournalDTO): int
     {
         return Journal::query()
@@ -66,6 +97,13 @@ class JournalService
         ;
     }
 
+    /**
+     * Delete a Journal record from the database belonging to the user requesting deletion
+     *
+     * @param \App\DTO\Journal\DeleteJournalDTO $deleteJournalDTO
+     *
+     * @return mixed 1 on success 0 on failure
+     */
     public function delete(DeleteJournalDTO $deleteJournalDTO)
     {
         return Journal::query()
--- src/Services/LoginService.php
+++ src/Services/LoginService.php
@@ -9,6 +9,8 @@ use App\Models\User;
 class LoginService
 {
     /**
+     * Logs the user in. Returns an instance of the User model on success or null on failure.
+     *
      * @throws \App\Exceptions\UserNotVerifiedException
      * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
      *
